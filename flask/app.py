75f24df204dae56c00a9dd0b9ac4238fa4596b5c
nur <nuraalamp@gmail.com>
2021-07-27 01:34:54 +0600

    session based

--- flask/app.py
+++ flask/app.py
@@ -4,12 +4,14 @@ import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app,logger=True, engineio_logger=True)
-# socketio = SocketIO(app)
+# socketio = SocketIO(app,logger=True, engineio_logger=True)
+socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('session.html')
+@app.route('/<string:username>')
+def index(username):
+    user=username
+    session["username"]=username
+    return render_template('session.html',user=username)
 
 @socketio.on('my event')
 def test_message(message):
@@ -44,7 +46,7 @@ def create_room(msg):
     print(roomname,roomID)
     room = _id
     join_room(room)
-    emit('private_room',{'roomID':room, "roomname":roomname,'username':msg['username']}, room=room)
+    emit('private_room',{'roomID':room, "roomname":roomname,'username':session['username']}, room=room)
     # send(username + ' has entered the room.'+room, to=room)
 
 
@@ -56,12 +58,14 @@ def join(msg):
     # roomID["room"]=msg['roomID']
     # print(roomname,roomID)
     _id = msg['roomID']
+    print("this s join id:",_id)
     # roomID[room].append(request.sid)
     if  roomID.get(roomname) !=_id :
         emit(roomname+" does not exist")
     else:
         key_session[request.sid]=_id
         join_room(_id)
+        print("ID check",request.sid,"/n",key_session[request.sid])
         emit('chat',{'data':roomname, "user":_id}, room=_id)
         # send(username + ' has entered the room.'+room, to=room)
 
@@ -71,7 +75,7 @@ def test_message(message):
     # room = message['room']
     print("here is the room",roomID)
     # if request.sid in roomID[_id]:
-    emit('chat', {'data': message['data'],"user":request.sid},room=key_session[request.sid])
+    emit('chat', {'data': message['data'],"user":session['username']},room=key_session[request.sid])
 
 
 @socketio.on('disconnect')
@@ -80,4 +84,4 @@ def test_disconnect():
     emit('my response', {'data': 'DisConnected'})
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True)
+    socketio.run(app)
